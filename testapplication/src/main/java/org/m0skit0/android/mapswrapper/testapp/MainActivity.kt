dfb1eda800e6bd9b11dedc94ac561074cb81ab37
m0skit0 <ametric@gmail.com>
2020-05-16 12:49:30 +0200

    Added test application

---
+++ testapplication/src/main/java/org/m0skit0/android/mapswrapper/testapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.m0skit0.android.mapswrapper.testapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
