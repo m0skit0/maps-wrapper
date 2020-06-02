0ebc20857b757b6c079dea2d1d2e217db7d95a30
m0skit0 <ametric@gmail.com>
2020-06-02 23:39:22 +0200

    CommonMap code refactor

--- maps-wrapper/src/main/java/org/m0skit0/android/mapswrapper/CommonMap.kt
+++ maps-wrapper/src/main/java/org/m0skit0/android/mapswrapper/CommonMap.kt
@@ -3,22 +3,19 @@ package org.m0skit0.android.mapswrapper
 import com.google.android.gms.maps.GoogleMap
 import com.huawei.hms.maps.HuaweiMap
 
-class CommonMap(map: Any) {
+class CommonMap(private val map: Any) {
 
-    private lateinit var google: GoogleMap
-    private lateinit var huawei: HuaweiMap
-    private lateinit var type: MapType
-
-    init {
-        when (map) {
-            is GoogleMap -> {
-                google = map
-                type = MapType.GOOGLE
-            }
-            is HuaweiMap -> {
-                huawei = map
-                type = MapType.HUAWEI
-            }
-        }
+    private var type: MapType = when (map) {
+        is GoogleMap -> MapType.GOOGLE
+        is HuaweiMap -> MapType.HUAWEI
+        else -> throw IllegalArgumentException("Constructor map argument is neither a Google Map nor a Huawei Map")
     }
+
+    private val googleMap: GoogleMap
+        get() = map as GoogleMap
+
+    private val huaweiMap: HuaweiMap
+        get() = map as HuaweiMap
+
+
 }
