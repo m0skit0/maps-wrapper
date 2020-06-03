70a6fe2ecc7502c6ffc70a5b57d1192d6738886b
m0skit0 <ametric@gmail.com>
2020-06-03 21:11:55 +0200

    Implemented CameraPosition wrapper

---
+++ maps-wrapper/src/main/java/org/m0skit0/android/mapswrapper/CameraPosition.kt
@@ -0,0 +1,47 @@
+package org.m0skit0.android.mapswrapper
+
+class CameraPosition private constructor(
+    internal val google: com.google.android.gms.maps.model.CameraPosition,
+    internal val huawei: com.huawei.hms.maps.model.CameraPosition
+) {
+
+    companion object {
+        fun builder(): Builder = Builder()
+        fun fromLatLngZoom(latLng: LatLng, zoom: Float): CameraPosition =
+            CameraPosition(
+                com.google.android.gms.maps.model.CameraPosition.fromLatLngZoom(latLng.google, zoom),
+                com.huawei.hms.maps.model.CameraPosition.fromLatLngZoom(latLng.huawei, zoom)
+            )
+    }
+
+    class Builder {
+
+        private var google = com.google.android.gms.maps.model.CameraPosition.Builder()
+        private var huawei = com.huawei.hms.maps.model.CameraPosition.Builder()
+
+        fun target(target: LatLng): Builder = apply {
+            google = google.target(target.google)
+            huawei = huawei.target(target.huawei)
+        }
+
+        fun zoom(zoom: Float): Builder = apply {
+            google = google.zoom(zoom)
+            huawei = huawei.zoom(zoom)
+        }
+
+        fun tilt(tilt: Float): Builder = apply {
+            google = google.tilt(tilt)
+            huawei = huawei.tilt(tilt)
+        }
+
+        fun bearing(bearing: Float): Builder = apply {
+            google = google.bearing(bearing)
+            huawei = huawei.bearing(bearing)
+        }
+
+        fun build(): CameraPosition =
+            CameraPosition(google.build(), huawei.build())
+    }
+
+
+}
