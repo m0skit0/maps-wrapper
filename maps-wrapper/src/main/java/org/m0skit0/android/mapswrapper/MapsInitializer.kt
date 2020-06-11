277292c8975f02dd159b215b9a280099d5ac8fca
m0skit0 <ametric@gmail.com>
2020-06-11 20:26:01 +0200

    Fixed Google maps import

--- maps-wrapper/src/main/java/org/m0skit0/android/mapswrapper/MapsInitializer.kt
+++ maps-wrapper/src/main/java/org/m0skit0/android/mapswrapper/MapsInitializer.kt
@@ -1,12 +1,11 @@
 package org.m0skit0.android.mapswrapper
 
 import android.content.Context
-import com.google.android.gms.maps.MapsInitializer
 
 object MapsInitializer {
 
     fun initialize(context: Context) {
-        executeOrNull { MapsInitializer.initialize(context) }
+        executeOrNull { com.google.android.gms.maps.MapsInitializer.initialize(context) }
         com.huawei.hms.maps.MapsInitializer.initialize(context)
     }
 }
