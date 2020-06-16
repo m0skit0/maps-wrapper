3177adc08b9d81a451d5c41c49ab5685863b56de
m0skit0 <ametric@gmail.com>
2020-06-16 13:27:28 +0200

    Added getGoogle() and getHuawei() functions

--- maps-wrapper/src/main/java/org/m0skit0/android/mapswrapper/di/KoinInterface.kt
+++ maps-wrapper/src/main/java/org/m0skit0/android/mapswrapper/di/KoinInterface.kt
@@ -2,7 +2,12 @@ package org.m0skit0.android.mapswrapper.di
 
 import org.koin.core.Koin
 import org.koin.core.KoinComponent
+import org.koin.core.get
+import org.koin.core.parameter.parametersOf
 
 internal interface KoinInterface : KoinComponent {
     override fun getKoin(): Koin = koin()
 }
+
+internal inline fun <reified T, U> KoinInterface.getGoogle(google: U): T = get { parametersOf(google, null) }
+internal inline fun <reified T, U> KoinInterface.getHuawei(huawei: U): T = get { parametersOf(null, huawei) }
