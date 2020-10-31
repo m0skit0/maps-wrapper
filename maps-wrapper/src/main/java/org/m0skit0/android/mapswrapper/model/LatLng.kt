e11b6d47f44844d915f6ac9f51954250fd631329
m0skit0 <ametric@gmail.com>
2020-10-31 21:55:04 +0100

    LatLng implements Parcelable

--- maps-wrapper/src/main/java/org/m0skit0/android/mapswrapper/model/LatLng.kt
+++ maps-wrapper/src/main/java/org/m0skit0/android/mapswrapper/model/LatLng.kt
@@ -1,6 +1,13 @@
 package org.m0skit0.android.mapswrapper.model
 
-data class LatLng(val latitude: Double, val longitude: Double) {
+import android.os.Parcelable
+import kotlinx.android.parcel.IgnoredOnParcel
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class LatLng(val latitude: Double, val longitude: Double) : Parcelable {
+    @IgnoredOnParcel
     internal val google by lazy { com.google.android.gms.maps.model.LatLng(latitude, longitude) }
+    @IgnoredOnParcel
     internal val huawei by lazy { com.huawei.hms.maps.model.LatLng(latitude, longitude) }
 }
