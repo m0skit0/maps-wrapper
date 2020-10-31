37b969c731283f9203abdeef82b9b31d13f8c1ac
m0skit0 <ametric@gmail.com>
2020-10-31 22:16:26 +0100

    Created TileOverlay and TileOverlayOptions

--- maps-wrapper/src/main/java/org/m0skit0/android/mapswrapper/model/TileOverlayOptions.kt
+++ maps-wrapper/src/main/java/org/m0skit0/android/mapswrapper/model/TileOverlayOptions.kt
@@ -1,4 +1,4 @@
-package org.m0skit0.android.mapswrapper
+package org.m0skit0.android.mapswrapper.model
 
 class TileOverlayOptions {
 
@@ -6,6 +6,22 @@ class TileOverlayOptions {
     internal var huawei = com.huawei.hms.maps.model.TileOverlayOptions()
 
     fun fadeIn(fadeIn: Boolean): TileOverlayOptions = apply {
+        google.fadeIn(fadeIn)
+        huawei.fadeIn(fadeIn)
+    }
 
+    fun transparency(transparency: Float): TileOverlayOptions = apply {
+        google.transparency(transparency)
+        huawei.transparency(transparency)
+    }
+
+    fun visible(isVisible: Boolean): TileOverlayOptions = apply {
+        google.visible(isVisible)
+        huawei.visible(isVisible)
+    }
+
+    fun zIndex(index: Float): TileOverlayOptions = apply {
+        google.zIndex(index)
+        huawei.zIndex(index)
     }
 }
