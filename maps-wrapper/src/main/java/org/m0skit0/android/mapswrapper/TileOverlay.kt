32ab034b477b912bc591624dd0659885223ff7dd
m0skit0 <ametric@gmail.com>
2020-11-01 18:45:52 +0100

    Removed duplicate classes

--- maps-wrapper/src/main/java/org/m0skit0/android/mapswrapper/TileOverlay.kt
+++
@@ -1,7 +0,0 @@
-package org.m0skit0.android.mapswrapper
-
-class TileOverlay(
-    internal val google: com.google.android.gms.maps.model.TileOverlay?,
-    internal val huawei: com.huawei.hms.maps.model.TileOverlay?
-) {
-}
