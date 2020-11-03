6d3caf8f6e85ee396ccd2f33ed697911ea4b5fa3
m0skit0 <ametric@gmail.com>
2020-11-03 12:28:34 +0100

    Added Projection visibleRegion and toScreenLocation()

--- maps-wrapper/src/main/java/org/m0skit0/android/mapswrapper/Projection.kt
+++ maps-wrapper/src/main/java/org/m0skit0/android/mapswrapper/Projection.kt
@@ -2,6 +2,8 @@ package org.m0skit0.android.mapswrapper
 
 import android.graphics.Point
 import org.m0skit0.android.mapswrapper.model.LatLng
+import org.m0skit0.android.mapswrapper.model.LatLngBounds
+import org.m0skit0.android.mapswrapper.model.VisibleRegion
 
 class Projection(
     internal val google: com.google.android.gms.maps.Projection?,
@@ -11,8 +13,32 @@ class Projection(
     internal constructor(google: com.google.android.gms.maps.Projection?) : this(google, null)
     internal constructor(huawei: com.huawei.hms.maps.Projection?) : this(null, huawei)
 
+    val visibleRegion: VisibleRegion
+        get() = google?.visibleRegion?.let {
+            VisibleRegion(
+                LatLng(it.nearLeft.latitude, it.nearLeft.longitude),
+                LatLng(it.nearRight.latitude, it.nearRight.longitude),
+                LatLng(it.farLeft.latitude, it.farLeft.longitude),
+                LatLng(it.farRight.latitude, it.farRight.longitude),
+                LatLngBounds(it.latLngBounds)
+            )
+        } ?: huawei?.visibleRegion?.let {
+            VisibleRegion(
+                LatLng(it.nearLeft.latitude, it.nearLeft.longitude),
+                LatLng(it.nearRight.latitude, it.nearRight.longitude),
+                LatLng(it.farLeft.latitude, it.farLeft.longitude),
+                LatLng(it.farRight.latitude, it.farRight.longitude),
+                LatLngBounds(it.latLngBounds)
+            )
+        } ?: throwUnableToResolveGoogleOrHuawei()
+
     fun fromScreenLocation(point: Point): LatLng =
         google?.fromScreenLocation(point)?.let { LatLng(it.latitude, it.longitude) }
             ?: huawei?.fromScreenLocation(point)?.let { LatLng(it.latitude, it.longitude) }
-            ?: LatLng(0.0, 0.0)
+            ?: throwUnableToResolveGoogleOrHuawei()
+
+    fun toScreenLocation(location: LatLng): Point =
+        google?.toScreenLocation(location.google)
+            ?: huawei?.toScreenLocation(location.huawei)
+            ?: throwUnableToResolveGoogleOrHuawei()
 }
