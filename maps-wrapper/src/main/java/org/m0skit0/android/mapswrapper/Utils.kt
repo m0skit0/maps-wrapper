7b10954e789211ea15c77071c16f65c0e3849973
m0skit0 <ametric@gmail.com>
2021-04-27 11:57:24 +0200

    Log the exception on executeOrNull lambda wrapper

--- maps-wrapper/src/main/java/org/m0skit0/android/mapswrapper/Utils.kt
+++ maps-wrapper/src/main/java/org/m0skit0/android/mapswrapper/Utils.kt
@@ -1,6 +1,12 @@
 package org.m0skit0.android.mapswrapper
 
-internal fun <T> executeOrNull(block: () -> T): T? = try { block() } catch (e: Exception) { null }
+internal fun <T> executeOrNull(block: () -> T): T? =
+    try {
+        block()
+    } catch (e: Exception) {
+        e.printStackTrace()
+        null
+    }
 
 internal fun throwUnableToResolveGoogleOrHuawei(): Nothing =
     throw IllegalStateException("Could not resolve if Google or Huawei")
