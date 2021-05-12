95b1c4505ff95fcf487b10ad8732c1a2e88800c6
m0skit0 <ametric@gmail.com>
2021-05-12 17:16:31 +0200

    Added Firebase Analytics so we can send the crash to Crashlytics
    Added Firebase Crashlytics so we can send the exception to Crashlytics

--- maps-wrapper/src/main/java/org/m0skit0/android/mapswrapper/Utils.kt
+++ maps-wrapper/src/main/java/org/m0skit0/android/mapswrapper/Utils.kt
@@ -1,10 +1,13 @@
 package org.m0skit0.android.mapswrapper
 
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+
 internal fun <T> executeOrNull(block: () -> T): T? =
     try {
         block()
     } catch (e: Exception) {
         e.printStackTrace()
+        FirebaseCrashlytics.getInstance().recordException(e)
         null
     }
 
